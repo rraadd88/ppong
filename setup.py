7677f7065dc934f2ad121c00681e74dae81e6044
rraadd88 <rohanadandage@gmail.com>
2020-05-16 20:25:17 -0400

    version bump

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
 name='ppong',
 author='Rohan Dandage',
 author_email='rohanadandage@gmail.com',
-version='0.0.1',
+version='0.0.2',
 url='https://github.com/rraadd88/ppong',
 download_url='https://github.com/rraadd88/ppong/archive/master.zip',
 description='ppong project',
